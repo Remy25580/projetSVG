ae4ef1f2a5efddfc2c522db1a7b5d7ad63ab3873
Remy <remymartinot9@gmail.com>
2025-10-07 15:56:05 +0200

    feat: adding the operations structures in path.h

--- creation/path.h
+++ creation/path.h
@@ -1,5 +1,46 @@
 #ifndef PATH
 #define PATH
 
+typedef struct{
+    
+} M;
+
+typedef struct{
+
+} L;
+
+typedef struct{
+
+} H;
+
+typedef struct{
+
+} V;
+
+typedef struct{
+
+} Z;
+
+typedef struct{
+
+} C;
+
+typedef struct{
+
+} S;
+
+typedef struct{
+
+} Q;
+
+typedef struct{
+
+} T;
+
+typedef struct{
+
+} A;
+
+
 
 #endif
