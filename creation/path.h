879cf2d7244b94a0cf34ca5a9b7284a5f47653cd
Remy <remymartinot9@gmail.com>
2025-10-07 16:16:06 +0200

    feat: filling the other structures

--- creation/path.h
+++ creation/path.h
@@ -2,21 +2,21 @@
 #define PATH
 
 typedef struct{
-    int xpos;
-    int ypos;
+    float xpos;
+    float ypos;
 } M;
 
 typedef struct{
-    int xpos;
-    int ypos;
+    float xpos;
+    float ypos;
 } L;
 
 typedef struct{
-    int xpos
+    float xpos
 } H;
 
 typedef struct{
-    int ypos;
+    float ypos;
 } V;
 
 typedef struct{
@@ -24,23 +24,41 @@ typedef struct{
 } Z;
 
 typedef struct{
-    
+    float xpent1;
+    float ypent1;
+    float xpent2;
+    float ypent2;
+    float xend;
+    float yend;
 } C;
 
 typedef struct{
-
+    float xpent2;
+    float ypent2;
+    float xend;
+    float yend;
 } S;
 
 typedef struct{
-
+    float xpent1;
+    float ypent1;
+    float xend;
+    float yend;
 } Q;
 
 typedef struct{
-
+    float xend;
+    float yend;
 } T;
 
 typedef struct{
-
+    float xradius;
+    float yradius;
+    float rotation;
+    int side;
+    int drawingWay;
+    float xend;
+    float yend;
 } A;
 
 
