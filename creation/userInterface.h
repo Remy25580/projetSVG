48ffbcfe418480fe2d7f56201e200bd6361e33e0
Remy <remymartinot9@gmail.com>
2025-10-10 10:07:49 +0200

    feat: adding the created forms to the shapes union

--- creation/userInterface.h
+++ creation/userInterface.h
@@ -1,4 +1,14 @@
 #ifndef USER
 #define USER
 
+typedef union{
+    rectangle_t rectangle;
+    square_t square;
+    circle_t circle;
+    ellipse_t ellipse;
+    line_t line;
+    polyline_t polyline;
+    polygon_t polygon;
+    path_t path;
+} shapes;
 #endif
