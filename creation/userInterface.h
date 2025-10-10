ac798dd499920016ec5b7fd097d09f6f16682dc0
Remy <remymartinot9@gmail.com>
2025-10-10 10:47:06 +0200

    feat: finalizing the structures

--- creation/userInterface.h
+++ creation/userInterface.h
@@ -1,6 +1,10 @@
 #ifndef USER
 #define USER
 
+typedef struct shapesElt_s shapesElt;
+typedef struct shapeGroup_s shapeGroup_t;
+
+
 typedef union{
     rectangle_t rectangle;
     square_t square;
@@ -10,5 +14,33 @@ typedef union{
     polyline_t polyline;
     polygon_t polygon;
     path_t path;
+    shapeGroup_t group;
 } shapes;
+
+typedef enum{
+    RECTANGLETYPE,
+    SQUARETYPE,
+    CIRCLETYPE,
+    ELLIPSETYPE,
+    LINETYPE,
+    POLYLINETYPE,
+    POLYGONTYPE,
+    PATHTYPE,
+    GROUPTYPE
+} shapesType;
+
+typedef struct{
+    char *name;
+    shapes shp;
+    shapesType shpType;
+} shapeElt;
+
+typedef struct{
+    shapeElt *list;
+}shapeGroup_t;
+
+
+int user(void);
+shapeGroup_t shapeCreation(void);
+
 #endif
