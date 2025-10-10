facde6971a99b9c578ba3744e3fb016bd028977d
Remy <remymartinot9@gmail.com>
2025-10-10 14:03:33 +0200

    feat: adding those features in main.c

--- main.c
+++ main.c
@@ -5,21 +5,32 @@
 #include "creation/userInterface.h"
 
 int main(void){
+    int end = 0;
     int choice = 1;
+    int loaded = 0;
     printf("Welcome to that SVG editor.\n");
-    while(choice > 0 && choice < 6){
+    while(end == 0){
         printf("What do you want to do ?\n");
         printf("1 - Create new shapes\n");
         printf("2 - Edit shapes\n");
         printf("3 - Save shapes\n");
         printf("4 - Load shapes\n");
         printf("5 - View your shapes\n");
+        printf("6 - Delete a shape\n");
         printf("0 - Exit\n");
         printf("\n");
 
         scanf("%d", &choice);
         switch (choice){
             case(1):
+                if (loaded == 0){
+                    loaded = 1;
+                    shapeGroup_t newCreation;
+                    shapeCreation(newCreation);
+                }
+                else{
+                    printf("A creation space has already been created or loaded\n");
+                }
                 break;
             case(2):
                 break;
@@ -29,9 +40,13 @@ int main(void){
                 break;
             case(5):
                 break;
+            case(6):
+                break;
             case(0):
+                end =1;
                 break;
             default:
+                printf("Invalid action, please retype.\n");
                 break;
         }
     }
