f3025ad0554b4b3dd68d30154f83256312632ca8
Remy <remymartinot9@gmail.com>
2025-10-13 11:18:13 +0200

    fix: trying to fix the name duplicata bug

--- creation/userInterface.c
+++ creation/userInterface.c
@@ -8,11 +8,12 @@ shapeGroup_t *shapeCreation(shapeGroup_t *g){
     int end = 0;
     int err;
     int creation = 0;
-    shapesElt temp;
+    shapesElt *temp;
     char *name = malloc(sizeof(char) * 20);
 
     
     while(end == 0){
+        temp = malloc(1* sizeof(shapesElt));
         err = 0;
         printf("What shape do you want to make ?\n");
         printf("1 - rectangle\n");
@@ -29,75 +30,75 @@ shapeGroup_t *shapeCreation(shapeGroup_t *g){
         scanf("%d", &creation);
         switch (creation){
             case(1):
-                temp.shpType = RECTANGLETYPE;
-                temp.shp.rectangle = createRectangle();
+                temp->shpType = RECTANGLETYPE;
+                temp->shp.rectangle = createRectangle();
                 printf("How do you want to name your rectangle ?(20 characters maximum)\n");
                 scanf("%19s", name);
-                temp.name = name;
+                temp->name = name;
                 printf("\n");
                 break;
             case(2):
-                temp.shpType = SQUARETYPE;
-                temp.shp.square = createSquare();
+                temp->shpType = SQUARETYPE;
+                temp->shp.square = createSquare();
                 printf("How do you want to name your square ?(20 characters maximum)\n");
                 scanf("%19s", name);
-                temp.name = name;
+                temp->name = name;
                 printf("\n");
                 break;
             case(3):
-                temp.shpType = CIRCLETYPE;
-                temp.shp.circle = createCicrcle();
+                temp->shpType = CIRCLETYPE;
+                temp->shp.circle = createCicrcle();
                 printf("How do you want to name your circle ?(20 characters maximum)\n");
                 scanf("%19s", name);
-                temp.name = name;
+                temp->name = name;
                 printf("\n");
                 break;
             case(4):
-                temp.shpType = ELLIPSETYPE;
-                temp.shp.ellipse = createEllipse();
+                temp->shpType = ELLIPSETYPE;
+                temp->shp.ellipse = createEllipse();
                 printf("How do you want to name your ellipse ?(20 characters maximum)\n");
                 scanf("%19s", name);
-                temp.name = name;
+                temp->name = name;
                 printf("\n");
                 break;
             case(5):
-                temp.shpType = LINETYPE;
-                temp.shp.line = createLine();
+                temp->shpType = LINETYPE;
+                temp->shp.line = createLine();
                 printf("How do you want to name your line ?(20 characters maximum)\n");
                 scanf("%19s", name);
-                temp.name = name;
+                temp->name = name;
                 printf("\n");
                 break;
             case(6):
-                temp.shpType = POLYLINETYPE;
-                temp.shp.polyline = createPolyline();
+                temp->shpType = POLYLINETYPE;
+                temp->shp.polyline = createPolyline();
                 printf("How do you want to name your polyline ?(20 characters maximum)\n");
                 scanf("%19s", name);
-                temp.name = name;
+                temp->name = name;
                 printf("\n");
                 break;
             case(7):
-                temp.shpType = POLYGONTYPE;
-                temp.shp.polygon = createPolygon();
+                temp->shpType = POLYGONTYPE;
+                temp->shp.polygon = createPolygon();
                 printf("How do you want to name your polygon ?(20 characters maximum)\n");
                 scanf("%19s", name);
-                temp.name = name;
+                temp->name = name;
                 printf("\n");
                 break;
             case(8):
-                temp.shpType = PATHTYPE;
-                temp.shp.path = createPath();
+                temp->shpType = PATHTYPE;
+                temp->shp.path = createPath();
                 printf("How do you want to name your path ?(20 characters maximum)\n");
                 scanf("%19s", name);
-                temp.name = name;
+                temp->name = name;
                 printf("\n");
                 break;
             case(9):
-                temp.shpType = GROUPTYPE;
-                temp.shp.group = shapeCreation(g);
+                temp->shpType = GROUPTYPE;
+                temp->shp.group = shapeCreation(g);
                 printf("How do you want to name your group ?(20 characters maximum)\n");
                 scanf("%19s", name);
-                temp.name = name;
+                temp->name = name;
                 printf("\n");
                 break;
             case(0):
@@ -115,13 +116,13 @@ shapeGroup_t *shapeCreation(shapeGroup_t *g){
             printf("nb de formes incremente\n");
             if (g->nb == 1){
                 g->list = malloc(1 * sizeof(shapesElt));
-                g->list[0] = temp;
+                g->list[0] = *temp;
                 printf("premier ajout");
             }
         
             else{
                 g->list = realloc(g->list, g->nb * sizeof(shapesElt));
-                g->list[g->nb - 1] = temp;
+                g->list[g->nb - 1] = *temp;
                 printf("nouvel ajout");
             }
         }
