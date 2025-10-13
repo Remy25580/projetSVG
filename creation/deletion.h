9e38aea952b7cf1af1c174b0bc0583ae356040fb
Remy <remymartinot9@gmail.com>
2025-10-13 11:38:25 +0200

    feat: initializing the deletion functions in deletion.h

--- creation/deletion.h
+++ creation/deletion.h
@@ -1,4 +1,14 @@
 #ifndef DELETION
 #define DELETION
 
+void freeRectangle(rectangle_t *rectangle);
+void freeSquare(square_t *square);
+void freeCircle(circle_t *circle);
+void freeEllipse(ellipse_t *ellipse);
+void freeLine(line_t *line);
+void freePolyline(polyline_t *polyline);
+void freePolygon(polygon_t *polygon);
+void freePath(path_t *path);
+void freeGroup(shapeGroup_t *group);
+
 #endif
