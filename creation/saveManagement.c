1847185bc7bfe9e875f7bdb11cdf93b3dc6840a7
Remy <remymartinot9@gmail.com>
2025-10-14 16:16:17 +0200

    feat: creating the base of the save function

--- creation/saveManagement.c
+++ creation/saveManagement.c
@@ -9,5 +9,39 @@
 #include "saveManagement.h"
 
 void saveShapes(shapeGroup_t *s){
-    
+    char fileNametext[20];
+    char fileNameSvg[20];
+    int choice = 2;
+    int valid = 0;
+
+    while(choice != 0 || choice != 1){
+        printf("Are you sure you want to save now ? 0 for no, 1 for yes\n");
+        scanf("%d", &choice);
+        if(choice == 0){
+            break;
+        }
+    }
+
+    if(choice == 1){
+        while(valid == 0){
+            printf("How do you want to name your save files ?\n");
+            printf("The text file : ");
+            scanf("%19s", fileNametext);
+            printf("The svg file : ");
+            scanf("%19s", fileNameSvg);
+
+            if(strstr(fileNametext, ".") != NULL || strstr(fileNameSvg, ".") != NULL){
+                printf("Invalid file name, you must not put a dot.\n");
+            }
+            else{
+                valid = 1;
+            }
+        }
+        strcat(fileNametext, ".txt");
+        strcar(fileNameSvg, ".svg");
+
+        FILE *ft = fopen(fileNametext, "w");
+        FILE *fs = fopen(fileNameSvg, "w");
+
+    }
 }
