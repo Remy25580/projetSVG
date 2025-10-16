78011639c76f2da99e746797d4306b57d21cd4d6
Remy <remymartinot9@gmail.com>
2025-10-16 18:37:43 +0200

    fix: adding the javadoc in saveManagement.h

--- creation/saveManagement.h
+++ creation/saveManagement.h
@@ -1,7 +1,18 @@
 #ifndef SAVE
 #define SAVE
 
+/**
+ * Saves the shapes created or loaded by writing it into a txt file
+ * @param s The shapes that are going to be saved
+ * @author Rémy Martinot
+ */
 void saveShapes(shapeGroup_t *s);
+
+/**
+ * Loads shapes from a txt file
+ * @param s The shape group in which the shapes are going to be loaded
+ * @author Rémy Martinot
+ */
 void loadShapes(shapeGroup_t *s);
 
 #endif
