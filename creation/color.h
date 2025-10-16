3d28595baf4422a0fb1b37e9895d64cccae153df
Remy <remymartinot9@gmail.com>
2025-10-16 16:44:24 +0200

    feat: javadoc of color.h

--- creation/color.h
+++ creation/color.h
@@ -1,6 +1,10 @@
 #ifndef COLOR
 #define COLOR
-
+/**
+ * Set the color for the styling of a shape
+ * @param s object of the structure shapeStyle, which contains the color
+ * @param fillOrContour define if the contour color is defined (0) or the filling color (1)
+ */
 void setColor(shapeStyle *s, int fillOrContour);
 
 #endif
